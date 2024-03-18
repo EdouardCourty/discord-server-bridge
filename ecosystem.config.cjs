a019794568a07d8b7665b04b9c5d9e855033cda7
Edouard Courty <edouard.courty2@gmail.com>
2024-03-18 02:17:03 +0100

    chore (deploy) add pm2 deployment script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
     apps: [{
         name: 'discord-server-bridge',
@@ -5,5 +7,18 @@ module.exports = {
         instances: 1,
         watch: false,
         max_memory_restart: '150M'
-    }]
+    }],
+    env_production: {
+        NODE_ENV: 'production'
+    },
+    deploy: {
+        production: {
+            'user': 'edouard',
+            'host': process.env.PM2_DEPLOY_IP,
+            'ref': 'origin/master',
+            'repo': 'git@github.com:EdouardCourty/discord-server-bridge',
+            'path': '/var/www/discord-server-bridge',
+            'post-deploy': 'yarn install && pm2 startOrRestart ecosystem.config.cjs --env production'
+        }
+    }
 }
